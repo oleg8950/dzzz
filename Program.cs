cf5568321e84fcb70ce51b71b25c42f6decaf2e0
Oleg <skipper8950@gmail.com>
2020-12-18 22:54:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "How are you?";
+    
+            File.WriteAllText(@"C:\Users\User\Desktop/textfile", text);
+
+            Console.WriteLine(Text was written)
+            Console.ReadKey();
+        }
+    }
+}
